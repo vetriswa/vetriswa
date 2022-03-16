ec82617ccc0db8a21623cf4d82e28e159b2f9ca6
vetriswa <vetriswa@gmail.com>
2022-03-16 12:26:06 +0530

    modify

--- demo/first.js
+++ demo/first.js
@@ -1,32 +1,22 @@
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-//document.title=123;
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-//document.all[10].textContent = 'hello';
-console.log(document.form);
-console.log(document.links);
-console.log(document.images);
+//var header = document.querySelector('#main-header');
+//header.style.borderBottom = 'solid 4px #ccc';
+var input = document.querySelector('input');
+input.value = "hello world";
+//var submit = document.querySelector('input[type="sunmit"]');
+//submit.value = "SEND";
+var item = document.querySelector('.list-group-item');
+item.style.color = "red";
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'green';
+var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+thirdItem.style.display = 'none';
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'hello';
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
+for (var i = 0; i < odd.length; i++) {
+    odd[i].style.background = '#f4f4f4';
+    even[i].style.background = '#ccc';
 
-//console.log(document.getElementById('header-titlr'));
-var headerTitle = document.getElementById('header-title');
-console.log(headerTitle);
-//headerTitle.textContent = 'hello';
-//headerTitle.innerText = 'goodbye';
-//console.log(headerTitle.innerText);
-//headerTitle.innerHTML = '<h2>hello</h2>';
-//header.style.borderBottom = 'solid 3px #000';
-//var items = document.getElementsByClassName('list-group-item');
-//console.log(items);
-
-//item.style.backgroundColor = 'yellow';
-//for (var i = 0; i < items.length; i++) {
-//  item[i] = style.fontweight = 'bold';
-//}
-var li = document.getElementsByTagName('li');
-console.log(li);
-li[i].textContent = 'hello';
+}
