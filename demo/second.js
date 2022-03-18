9f75f3aa1fc995d1df13c361a1235715c950d1b9
vetriswa <vetriswa@gmail.com>
2022-03-18 22:22:37 +0530

    modify

--- demo/second.js
+++ demo/second.js
@@ -1,1 +1,31 @@
-console.log("second document");
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+//form submit event
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItem);
+
+
+//add item
+function addItem(e) {
+    e.preventDefault();
+    //console.log(1);
+
+    var newItem = document.getElementById('item').value;
+
+    var li = document.createElement('li');
+    li.className = 'list-group-item';
+    li.appendChild(document.createTextNode(newItem));
+    itemList.appendChild(l1);
+}
+
+function removeItem(e) {
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+    }
+}
