870b3ba990d370d08f14af2be36eb32e40eddd90
vetriswa <vetriswa@gmail.com>
2022-03-18 23:22:00 +0530

    modify

--- demo/second.js
+++ demo/second.js
@@ -5,7 +5,7 @@ var filter = document.getElementById('filter');
 //form submit event
 form.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
-filter.addEventListener('keyup', filterItem);
+filter.addEventListener('keyup', filterItems);
 
 
 //add item
@@ -28,4 +28,19 @@ function removeItem(e) {
             itemList.removeChild(li);
         }
     }
+}
+
+function filterItems(e) {
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    Array.from(items).forEach(function(item) {
+        var itemName = item.firstChild.textContent;
+        if (itemName.toLowerCase().indexOf(text) != -1) {
+            item.style.display = 'block';
+
+        } else {
+            item.style.display = 'none';
+        }
+    });
+
 }
