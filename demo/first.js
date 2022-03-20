659aedb4e6596a5737afe285059f706e92fa21f4
vetriswa <vetriswa@gmail.com>
2022-03-20 21:25:27 +0530

    second.js

--- demo/first.js
+++ demo/first.js
@@ -20,35 +20,36 @@
 //     even[i].style.background = '#ccc';
 
 //}
-var itemList = document.querySelector('#items');
-console.log(itemList.parentNode);
-itemList.parentNode.style.background = '#f4f4f4';
-var itemList = document.querySelector('#items');
-console.log(itemList.parentElement);
-itemList.parentElement.style.background = '#f4f4f4';
+// var itemList = document.querySelector('#items');
+// console.log(itemList.parentNode);
+// itemList.parentNode.style.background = '#f4f4f4';
+// var itemList = document.querySelector('#items');
+// console.log(itemList.parentElement);
+// itemList.parentElement.style.background = '#f4f4f4';
 
-console.log(itemList.childNodes);
-console.log(itemList.children[2]);
-itemList.children[1].style.background = 'yellow';
-console.log(itemList.child);
-console.log(itemList.firstElementChild);
-itemList.firstElementChild.textContent = 'hello';
-console.log(itemList.lastElementChild);
-itemList.lastElementChild.textContent = 'hello1';
-console.log(itemList.nextSibling);
-console.log(itemList.nextElementSibling);
-console.log(itemList.previousSibling);
-console.log(itemList.previousElementSibling);
-itemList.previousElementSibling.style.background = 'green';
+// console.log(itemList.childNodes);
+// console.log(itemList.children[2]);
+// itemList.children[1].style.background = 'yellow';
+// console.log(itemList.child);
+// console.log(itemList.firstElementChild);
+// itemList.firstElementChild.textContent = 'hello';
+// console.log(itemList.lastElementChild);
+// itemList.lastElementChild.textContent = 'hello1';
+// console.log(itemList.nextSibling);
+// console.log(itemList.nextElementSibling);
+// console.log(itemList.previousSibling);
+// console.log(itemList.previousElementSibling);
+// itemList.previousElementSibling.style.background = 'green';
 
-var newDiv = document.createElement('div');
-newDiv.className = 'hello3';
-newDiv.setAttribute('title', 'hello3 div')
+// var newDiv = document.createElement('div');
+// newDiv.className = 'hello3';
+// newDiv.id = 'hello4';
+// newDiv.setAttribute('title', 'hello3 div')
 
-var newDivText = document.createTextNode('hello world');
-newDiv.appendChild(newDivText);
-var container = document.querySelector('header.container');
-var h1 = document.querySelector('header h1');
-console.log(newDiv);
-newDiv.style.fontSize = '30px';
-container.insertBefore(newDiv, h1);
+// var newDivText = document.createTextNode('hello world');
+// newDiv.appendChild(newDivText);
+// var container = document.querySelector('header.container');
+// var h1 = document.querySelector('header h1');
+// console.log(newDiv);
+// newDiv.style.fontSize = '30px';
+// //container.insertBefore(newDiv, h1);
